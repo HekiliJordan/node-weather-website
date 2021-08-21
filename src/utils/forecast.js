e3957015ea71325c66473b4600a15f9823673247
Hekili Jordan <hekili.jordan@wsu.edu>
2021-08-20 17:52:06 -0700

    Added text to index page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,10 +1,11 @@
+const { debug } = require('request')
 const request = require('request')
 
 // forecast
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=dc2f62f26681809bc84786192411a1a6&query=' + latitude + ',' + longitude + '&units=f'
-
+    
     request ( {url, json: true}, (error,{body}) => {
         if (error){
             callback('Unable to connect to weather service!',undefined)
@@ -13,10 +14,15 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location! Try another search',undefined)
         }
         else{
+
+            debugger
+            let localtime = body.location.localtime
+            let time = localtime.split(" ")
+
             callback(undefined,
-               'The current weather forecast for today is ' + body.current.weather_descriptions[0].toLowerCase() + 
-               '. It is currently ' + body.current.temperature + ' degrees outside and it currently feels like ' + 
-               body.current.feelslike + ' degrees'
+               'Right now in ' + body.location.name + ': ' + body.current.weather_descriptions[0].toLowerCase() + ' outside' + 
+               '. The weather outside is ' + body.current.temperature + ' degrees with a precipitation level of ' + 
+               body.current.precip + '.The current time is ' + time[1]
             )}
         
     })
